1311824b376cc0ff297a108b2fa6826b384b7e85
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-02-23 06:39:18 +0530

    chore(backend): Renamed Todo to TodoItem

--- backend/src/main/java/com/learning/todoapp/model/TodoItem.java
+++ backend/src/main/java/com/learning/todoapp/model/TodoItem.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Todo {
+public class TodoItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- backend/src/main/java/com/learning/todoapp/repositories/TodoRepository.java
+++ backend/src/main/java/com/learning/todoapp/repositories/TodoRepository.java
@@ -3,11 +3,11 @@ package com.learning.todoapp.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.learning.todoapp.model.Todo;
+import com.learning.todoapp.model.TodoItem;
 
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<TodoItem, Long> {
 
-    
+
 
 }
